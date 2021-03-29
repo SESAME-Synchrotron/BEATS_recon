6b9249bb0b7367bc1ba2ede33b4e680668c0b0b1
gianthk <gianthk.iori@gmail.com>
2021-03-29 13:57:56 +0300

    COR find_center_vo`\

--- scripts/Cyclone/tomopy_testCyclone_recon_algorithms_comparison.py
+++ scripts/Cyclone/tomopy_testCyclone_recon_algorithms_comparison.py
@@ -1,4 +1,4 @@
-# full TomoPy recon test on Cyclone
+# TomoPy recon on Cyclone: compare different algorithms
 
 import tomopy
 import dxchange
@@ -31,11 +31,13 @@ def writemidplanesDxchange(data, filename_out):
         dxchange.writer.write_tiff(touint8(data[:, :, int(data.shape[2] / 2)]), fname=filename + '_YZ.tiff', dtype='uint8')
 
 h5file = "/tmp/tomoData/8671_8_B_01_/8671_8_B_01_.h5"
-path_recon = "/scratch/recon/8671_8_B_01_/"
-# path_recon = "/nvme/h/jo21gi1/data_p029/test_00_/recon_phase/"
+path_recon = "/scratch/recon/algorithm_test/"
 
-time_start = time()
-logging.basicConfig(filename=path_recon+'recon.log', level=logging.DEBUG)
+# path_recon = "/nvme/h/jo21gi1/data_p029/test_00_/recon_phase/"
+logging.basicConfig(filename=path_recon+'recon_algorithm_test.log', level=logging.DEBUG)
+
+
+CPU_algorithms = ['gridrec', 'fbp', 'mlem', 'sirt', 'art']
 
 # read projections, darks, flats and angles
 projs, flats, darks, theta = dxchange.read_aps_32id(h5file, exchange_rank=0)
@@ -53,36 +55,28 @@ projs = tomopy.normalize(projs, flats, darks)
 logging.info("- log transform.")
 projs = tomopy.minus_log(projs)
 
-# auto detect Center Of Rotation (COR)
-# logging.info("tomopy.find_center..")
-# COR = tomopy.find_center(projs, theta, init=projs.shape[2]/2, tol=1)
-
-# auto detect Center Of Rotation (COR) witn Vo method
-# logging.info("tomopy.find_center_vo..")
-# COR = tomopy.find_center_vo(projs)
-
 # COR was found with Vo method + manual inspection
 COR = 1303
 
-# CPU recon (GRIDREC)
-recon = tomopy.recon(projs, theta, center=COR, algorithm='gridrec', sinogram_order=False)
+for alg in CPU_algorithms:
+    time_start = time()
+    # CPU recon
+    recon = tomopy.recon(projs, theta, center=COR, algorithm=alg, sinogram_order=False)
+    time_end = time()
+    execution_time = time_end - time_start
+    logging.info("{} reconstructed in {} s".format(alg, str(execution_time)))
 
-# apply circular mask
-recon = tomopy.circ_mask(recon, axis=0, ratio=0.95)
+    # apply circular mask
+    recon = tomopy.circ_mask(recon, axis=0, ratio=0.95)
 
-# rescale GV range to uint8 from MIN and MAX of 3D data
-recon_uint8Range = touint8(recon)
+    # rescale GV range to uint8 from MIN and MAX of 3D data
+    recon_uint8Range = touint8(recon)
 
-# apply again circ mask
-recon_uint8Range = tomopy.circ_mask(recon_uint8Range, axis=0, ratio=0.95)
+    # apply again circ mask
+    recon_uint8Range = tomopy.circ_mask(recon_uint8Range, axis=0, ratio=0.95)
 
-# write output stack of TIFFs as uint8
-fileout = path_recon+'slice.tiff'
-dxchange.writer.write_tiff_stack(recon_uint8Range, fname=fileout, dtype='uint8', axis=0, digit=5, start=0, overwrite=True)
+    # write output stack of TIFFs as uint8
+    fileout = path_recon+alg
+    # dxchange.writer.write_tiff_stack(recon_uint8Range, fname=fileout, dtype='uint8', axis=0, digit=5, start=0, overwrite=True)
+    writemidplanesDxchange(recon_uint8Range, fileout)
 
-# write midplanes
-writemidplanesDxchange(recon_uint8Range, fileout)
-
-time_end = time()
-execution_time = time_end - time_start
-logging.info("Completed in {} s".format(str(execution_time)))
