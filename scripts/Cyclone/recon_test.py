c02eaf65cb0b3f7f22cdb71de81563b2353c0fb5
gianthk <gianthk.iori@gmail.com>
2021-06-19 16:52:31 +0300

    minor

--- scripts/Cyclone/recon_test.py
+++ scripts/Cyclone/recon_test.py
@@ -33,7 +33,7 @@ def writemidplanesDxchange(data, filename_out):
 # h5file = "/tmp/tomoData/8671_8_B_01_/8671_8_B_01_.h5"
 h5file = "/data/test_00_/test_00_.h5"
 
-path_recon = "/scratch/recon/algorithm_test2/gridrec"
+path_recon = "/scratch/recon/algorithm_test2/gridrec/"
 # path_recon = "/nvme/h/jo21gi1/data_p029/test_00_/recon_phase/"
 
 time_start = time()
