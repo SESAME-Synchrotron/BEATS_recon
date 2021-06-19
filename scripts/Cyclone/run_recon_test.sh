adbb5f50cbcf3ea75a9d4d02b4578b0b93366b4c
gianthk <gianthk.iori@gmail.com>
2021-06-19 17:15:18 +0300

    minor

--- scripts/Cyclone/run_recon_test.sh
+++ scripts/Cyclone/run_recon_test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#SBATCH -–nodes=1
-#SBATCH –-ntasks-per-node-10
+#SBATCH --nodes=1
+#SBATCH –-ntasks-per-node=10
 
 export SINGULARITY_HOME=$HOME:/home
 export SINGULARITY_BINDPATH="/onyx/data/p029:/data,/nvme/h/jo21gi1/code:/code,/nvme/h/jo21gi1/scratch:/scratch"
