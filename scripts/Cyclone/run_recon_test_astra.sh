fb4c27c224605ebb4ffadc6af758149a945ff8e8
gianthk <gianthk.iori@gmail.com>
2021-06-19 22:06:52 +0300

    testing recon_test_astra.py

--- scripts/Cyclone/run_recon_test_astra.sh
+++ scripts/Cyclone/run_recon_test_astra.sh
@@ -6,4 +6,4 @@
 export SINGULARITY_HOME=$HOME:/home
 export SINGULARITY_BINDPATH="/onyx/data/p029:/data,/nvme/h/jo21gi1/code:/code,/nvme/h/jo21gi1/scratch:/scratch"
 
-singularity exec -nv /nvme/h/jo21gi1/tomopy.sif python /code/tomopy_tests/scripts/Cyclone/recon_test_astra.py
+singularity exec --nv /nvme/h/jo21gi1/tomopy.sif python /code/tomopy_tests/scripts/Cyclone/recon_test_astra.py
