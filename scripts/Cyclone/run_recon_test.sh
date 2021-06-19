acbd1bac7adf5c8c067ed58d731af4bd415eb524
gianthk <gianthk.iori@gmail.com>
2021-06-19 16:41:04 +0300

    minor

--- scripts/Cyclone/run_recon_test.sh
+++ scripts/Cyclone/run_recon_test.sh
@@ -2,4 +2,4 @@
 export SINGULARITY_HOME=$HOME:/home
 export SINGULARITY_BINDPATH="/onyx/data/p029:/data,/nvme/h/jo21gi1/code:/code,/nvme/h/jo21gi1/scratch:/scratch"
 
-singularity exec /nvme/h/jo21gi1/tomopy.sif python /code/tomopy_tests/scripts/recon_test.py
+singularity exec /nvme/h/jo21gi1/tomopy.sif python /code/tomopy_tests/scripts/Cyclone/recon_test.py
