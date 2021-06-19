829e8768ba9dfd0b5702f1294418244c0864ce59
gianthk <gianthk.iori@gmail.com>
2021-06-19 22:01:56 +0300

    testing recon_test_astra.py

--- scripts/Cyclone/run_recon_test_astra.sh
+++ scripts/Cyclone/run_recon_test_astra.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-#SBATCH -–nodes=1
-#SBATCH -–gpus=1
+#SBATCH --nodes=1
+#SBATCH --gpus=1
+
 
 export SINGULARITY_HOME=$HOME:/home
 export SINGULARITY_BINDPATH="/onyx/data/p029:/data,/nvme/h/jo21gi1/code:/code,/nvme/h/jo21gi1/scratch:/scratch"
