c1b9629950aeff6282c92f98c2892b89efb88226
beats <gianluca.iori@sesame.org.jo>
2023-07-30 17:26:15 +0300

    investigating rum gpunode crash

--- tests/rum/BEATS_recon_test_phase.sh
+++ tests/rum/BEATS_recon_test_phase.sh
@@ -1,7 +1,8 @@
 # 30.07.2023 - investigating slurm crash on Rum nodes
 
 # python call for BL-BEATS-WS01
-# OK python /home/beats/PycharmProjects/BEATS_recon/scripts/rum/BEATS_recon.py /mnt/PETRA/SED/BEATS/IH/fiber_wet_below_kink_HR-20230726T171211/fiber_wet_below_kink_HR-20230726T171211.h5 -s 1000 1200 --recon_dir /home/beats/Data/AlHandawi/fiber_wet_below_kink_HR-20230726T171211/recon_phase_alpha0.0002/ --work_dir /home/beats/Data/AlHandawi/fiber_wet_below_kink_HR-20230726T171211/ --cor 718.5 --ncore 36 --phase --alpha 0.0002 --pixelsize 0.00065 --sdd 20
+# python /home/beats/PycharmProjects/BEATS_recon/scripts/rum/BEATS_recon.py /mnt/PETRA/SED/BEATS/IH/fiber_wet_below_kink_HR-20230726T171211/fiber_wet_below_kink_HR-20230726T171211.h5 -s 1000 1200 --recon_dir /home/beats/Data/AlHandawi/fiber_wet_below_kink_HR-20230726T171211/recon_phase_alpha0.0002/ --work_dir /home/beats/Data/AlHandawi/fiber_wet_below_kink_HR-20230726T171211/ --cor 718.5 --ncore 36 --phase --alpha 0.0002 --pixelsize 0.00065 --sdd 20
+# this call is successful. executed in < 1 min
 
 # python call for Rum
 # Modules section:
