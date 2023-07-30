d17313b29c958cee6426ae9544bd4c54c5e458c7
beats <gianluca.iori@sesame.org.jo>
2023-07-30 18:15:04 +0300

    report on rum crash

--- tests/rum/BEATS_recon_test_phase.sh
+++ tests/rum/BEATS_recon_test_phase.sh
@@ -1,8 +1,8 @@
 # 30.07.2023 - investigating slurm crash on Rum nodes
 
 # python call for BL-BEATS-WS01
-python /home/beats/PycharmProjects/BEATS_recon/scripts/rum/BEATS_recon.py /mnt/PETRA/SED/BEATS/IH/fiber_wet_below_kink_HR-20230726T171211/fiber_wet_below_kink_HR-20230726T171211.h5 -s 1000 1200 --recon_dir /home/beats/Data/AlHandawi/fiber_wet_below_kink_HR-20230726T171211/recon_phase_alpha0.0002/ --work_dir /home/beats/Data/AlHandawi/fiber_wet_below_kink_HR-20230726T171211/ --cor 718.5 --ncore 36 --phase --alpha 0.0002 --pixelsize 0.00065 --sdd 20
-# this call is successful. executed in < 1 min
+python /home/beats/PycharmProjects/BEATS_recon/scripts/rum/BEATS_recon.py /mnt/PETRA/SED/BEATS/IH/fiber_wet_below_kink_HR-20230726T171211/fiber_wet_below_kink_HR-20230726T171211.h5 -s 1000 1200 --recon_dir /home/beats/Data/AlHandawi/fiber_wet_below_kink_HR-20230726T171211/recon_phase_alpha0.0002/ --work_dir /home/beats/Data/AlHandawi/fiber_wet_below_kink_HR-20230726T171211/ --cor 718.5 --ncore 36 --phase --no-phase_pad --alpha 0.0002 --pixelsize 0.00065 --sdd 20
+# this call is successful for both '--phase_pad' and '--no-phase_pad'. executed in < 1 min
 
 # python call for Rum
 # Modules section:
@@ -17,7 +17,8 @@ python /PETRA/SED/BEATS/IH/scratch/scripts/BEATS_recon.py /PETRA/SED/BEATS/IH/fi
 
 # Phase-reconstruction
 python /PETRA/SED/BEATS/IH/scratch/scripts/BEATS_recon.py /PETRA/SED/BEATS/IH/fiber_wet_below_kink_HR-20230726T171211/fiber_wet_below_kink_HR-20230726T171211.h5 -s 1000 1200 --recon_dir /PETRA/SED/BEATS/IH/scratch/AlHandawi/fiber_wet_below_kink_HR-20230726T171211/recon_phase_alpha0.0002/ --work_dir /PETRA/SED/BEATS/IH/scratch/AlHandawi/fiber_wet_below_kink_HR-20230726T171211/ --cor 718.5 --ncore 36 --phase --no-phase_pad --alpha 0.0002 --pixelsize 0.00065 --sdd 20
-# this call is successful on cpunode. executed in < 2 sec
+# with `--phase_pad` this call is successful on cpunode. executed in < 2 sec
+# with `--no-phase_pad` this call is stuck infinitely on cpunode. Even killing python processes one by one does not help
 
 # Notes:
 # - !!!!!! Any python call on gpunode1 returns "Bus error (core dumped)"
