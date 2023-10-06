455d9dfc0c685d9bb57887f663f4e61b68888323
gianthk <gianthk.iori@gmail.com>
2023-10-06 13:40:06 +0300

    read init settings from flat YAML dict

--- examples/solara/test_class/alrecon.py
+++ examples/solara/test_class/alrecon.py
@@ -12,11 +12,13 @@ class alrecon:
 			alrecon_settings = yaml.load(file_object, Loader=yaml.SafeLoader)
 
 			# some app settings
-			self.pixelsize = solara.reactive(alrecon_settings['phase-retrieval']['pixelsize'])
+			# self.pixelsize = solara.reactive(alrecon_settings['phase-retrieval']['pixelsize'])
+			for key, val in alrecon_settings['phase-retrieval'].items():
+				print('key: {0}; val: {1}'.format(key, val))
+				exec('self.'+key + '=solara.reactive(val)')
 
 		print('Init settings file: {0}'.format(filename))
 		print('Init pixelsize: {0}'.format(self.pixelsize))
-
 
 	def load_app_settings(self, filename):
 		with open(filename, "r") as file_object:
