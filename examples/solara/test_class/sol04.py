17cc43c5518e3183a93d45a1aaa4b9ac051d5c57
gianthk <gianthk.iori@gmail.com>
2023-10-07 20:50:40 +0300

    minor

--- examples/solara/test_class/sol04.py
+++ examples/solara/test_class/sol04.py
@@ -12,8 +12,11 @@ def PhaseRetrieval(pippo):
 			solara.Markdown("pixelsize: {0}".format(pippo.pixelsize.value))
 			solara.Button(label="Load settings 1", on_click=lambda: pippo.load_app_settings(settings_file1))
 			solara.Button(label="Load settings 2", on_click=lambda: pippo.load_app_settings(settings_file2))
+			solara.Switch(label="Hide Message", value=pippo.phase_object)
 
 			solara.InputFloat("Enter pixelsize: ", value=pippo.pixelsize, continuous_update=False)
+			solara.Markdown("Phase object: {0}".format(pippo.phase_object.value))
+
 
 # pippo.load_app_settings('beats.yml')
 pippo = alrecon()
